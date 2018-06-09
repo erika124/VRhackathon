cdb63ca4a0aada1cf7bb37083b6cc6fbf42b2967
kanechiru <kanekoyuuto@gmail.com>
2018-06-09 18:11:35 +0900

    Add EventScript

---
+++ Assets/Scripts/Event/EventBaseObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class EventBaseObject : MonoBehaviour
+{
+	public virtual void DoEvent(GameObject HitObject){}
+
+	public void OnCollisionEnter(Collision other)
+	{
+		DoEvent(other.gameObject);
+	}
+}
---
+++ Assets/Scripts/Event/SeaEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaEvent : EventBaseObject
+{
+	public Material material;
+	private Status status;
+	enum Status{
+		SEA,
+		COLA
+	}
+
+	void Start()
+	{
+		status = Status.SEA;
+	}
+	
+	public override void DoEvent(GameObject HitObject)
+	{
+		if(HitObject.gameObject.tag == "Mentosu" && status == Status.SEA)
+		{
+			//マテリアルの色を徐々に変化する
+			GenerateBubble();
+			//30秒後にマテリアルの色が徐々に元になっていくようにセットをする
+		}
+	}
+
+	//泡を発生する
+	private void GenerateBubble()
+	{
+		//カメラが揺れる
+	}
+}
