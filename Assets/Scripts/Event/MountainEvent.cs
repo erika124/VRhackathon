3ffe7449bf8502181bd94b4677cb483d5449ed15
oshimaharuna <ooooooshima115@gmail.com>
2018-06-09 21:13:14 +0900

    Moutainアクション部分まで

--- Assets/Scripts/Event/MountainEvent.cs
+++ Assets/Scripts/Event/MountainEvent.cs
@@ -7,6 +7,7 @@ public class MountainEvent : EventBaseObject
 	//public Material material;
 	private Status status;
 	private GameObject fryingpan;
+		
 	enum Status{
 		MOUNTAIN,
 		FRYINGPAN
@@ -15,17 +16,34 @@ public class MountainEvent : EventBaseObject
 	{	
 		status = Status.MOUNTAIN;
 	}
+
+	
 	public override void DoEvent(GameObject HitObject)
 	{
-		if(HitObject.gameObject.tag == "fryingpan" && status == Status.MOUNTAIN)
+		HitObject.gameObject.transform.position = new Vector3(0,4,0);
+		if(HitObject.gameObject.tag == "Coin" && status == Status.MOUNTAIN){
+			//coinイベント開始	
+			Invoke ("Coin", 3);
+		}
+		else if(HitObject.gameObject.tag == "Fryingpan" && status == Status.MOUNTAIN)
 		{
 			//フライパンを山に設置
 			status = Status.FRYINGPAN;
-			//fryingpan = HitObject as GameObject;
-			//fryingpan.AddComponent< MountainEvent >().status = Status.FRYINGPAN;
 		}
-		else if(HitObject.gameObject.tag == "corn" && status == Status.FRYINGPAN){
-			//
+		else if(HitObject.gameObject.tag == "Corn" && status == Status.FRYINGPAN){
+			//popcornイベント開始
+			Invoke ("Popcorn", 3);
 		}
+		else{
+			//何も起こらない　キャラのセリフ誘発？
+			Destroy(HitObject,2);
+		}
+	}
+
+	private void Popcorn(){
+		Debug.Log("popcorn event start!");
+	}
+	private void Coin(){
+		Debug.Log("coin event start!");
 	}
 }
---
+++ Assets/Scripts/oshima.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oshima : MonoBehaviour {
+
+	public GameObject fryingpan;
+	public GameObject cola;
+	public GameObject corn;
+	public GameObject mentos;
+	public GameObject coin;
+
+	
+
+	Quaternion _rotation;
+	// Use this for initialization
+	void Start () {
+		_rotation.eulerAngles =new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F)){
+			GameObject f = Instantiate(fryingpan,new Vector3(-2,10,0),_rotation);
+			}
+		else if(Input.GetKeyDown(KeyCode.P)){
+			GameObject p = Instantiate(corn,new Vector3(-2,10,0),_rotation);
+			}
+		else if(Input.GetKeyDown(KeyCode.C)){
+			GameObject c = Instantiate(coin,new Vector3(-2,10,0),_rotation);
+			}
+	}
+}
