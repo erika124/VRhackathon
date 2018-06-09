1eb39e4e0fedf0d2cd4d077a304dbc4caa0027a2
kanechiru <kanekoyuuto@gmail.com>
2018-06-09 19:35:27 +0900

    UI

---
+++ Assets/ItemModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class ItemModel : MonoBehaviour
+{
+	[SerializeField]
+	private Button button;
+	[SerializeField]
+	private Text name;
+	[SerializeField]
+	private Text description;
+	[SerializeField]
+	private Sprite sprite;
+
+	void Start()
+	{
+		Item item = GetComponent<Item>();
+		name.text = item.name;
+		description.text = item.description;
+	}
+}
--- Assets/Scripts/Item/UIManager.cs
+++ Assets/Scripts/Item/UIManager.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class UIManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 		
 	}
 }
