cf90f49876d36be67f5322dc94625b564a27e481
kanechiru <kanekoyuuto@gmail.com>
2018-06-09 22:27:42 +0900

    UI周り

--- Assets/Scripts/Charater/CharacterManager.cs
+++ Assets/Scripts/Charater/CharacterManager.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	[SerializeField]
+	PlayerManager playerManager;
 }
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UniRx;
 public class PlayerManager : MonoBehaviour {
+	ReactiveProperty<GameObject> HasGameObject;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void GenerateObject(GameObject Obj)
+	{
+		GameObject obj = Instantiate(Obj);
 	}
 }
